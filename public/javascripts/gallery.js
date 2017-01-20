1e6474d0c71e548ee3b4590b0f522c0e7c7fb3b7
David Besserman <david.besserman@gmail.com>
2017-01-20 17:21:22 +0100

    slideshow functionality

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -12,6 +12,49 @@ $(function() {
     Handlebars.registerPartial($partial.attr('id'), $partial.html());
   });
 
+  var slideshow = {
+    $el: $('#slideshow'),
+    duration: 500,
+    prevSlide: function(e) {
+      e.preventDefault();
+      var $current = this.$el.find('figure:visible'),
+          $prev = $current.prev('figure');
+
+      if (!$prev.length) {
+        $prev = this.$el.find('figure').last();
+      } 
+
+      $current.fadeOut(this.duration);
+      $prev.fadeIn(this.duration);
+      this.renderPhotoContent($prev.data('id'));
+    },
+    nextSlide: function(e) {
+      e.preventDefault();
+      var $current = this.$el.find('figure:visible'),
+          $next = $current.next('figure');
+
+      if (!$next.length) {
+        $next = this.$el.find('figure').first();
+      } 
+
+      $current.fadeOut(this.duration);
+      $next.fadeIn(this.duration);
+      this.renderPhotoContent($next.data('id'));
+    },
+    renderPhotoContent: function(i) {
+      renderPhotoInformation(+i);
+      getCommentsFor(i);
+    },
+    bind: function() {
+      this.$el.find('a.prev').on('click', this.prevSlide.bind(this));
+      this.$el.find('a.next').on('click', this.nextSlide.bind(this));
+    },
+    init: function() {
+      this.bind();
+    },
+  }
+
+
   $.ajax({
     url: '/photos',
     data: 'JSON',
@@ -19,8 +62,9 @@ $(function() {
       photos = json;
 
       renderPhotos();
-      renderPhotoInformation(0);
+      renderPhotoInformation(photos[0].id);
       getCommentsFor(photos[0].id);
+      slideshow.init();
     },
     error: function() {
       console.log('AJAX request failed');
@@ -28,11 +72,14 @@ $(function() {
   });
 
   function renderPhotos() {
-    $('#slides').append(templates.photos({photos: photos}));
+    $('#slides').html(templates.photos({photos: photos}));
   }
 
-  function renderPhotoInformation(id) {
-    $('section > header').append(templates.photo_information(photos[id]));
+  function renderPhotoInformation(i) {
+    var photo = photos.filter(function(item) {
+      return item.id === i;
+    })[0];
+    $('section > header').html(templates.photo_information(photo));
   }
 
   function getCommentsFor(i) {
