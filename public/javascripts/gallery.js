99d11b8c2b3e9c520edde930092154e4bf857f9d
David Besserman <dbesserman@apollo.home>
2017-01-20 15:30:08 +0100

    render comments for the first photo

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -7,16 +7,45 @@ $(function() {
     templates[$template.attr('id')] = Handlebars.compile($template.html());
   });
 
+  $("[data-type='partial']").each(function() {
+    var $partial = $(this);
+    Handlebars.registerPartial($partial.attr('id'), $partial.html());
+  });
+
   $.ajax({
     url: '/photos',
     data: 'JSON',
     success: function(json) {
       photos = json;
-      $('#slides').append(templates.photos({photos: photos}));
-      $('section > header').append(templates.photo_information(photos[0]));
+
+      renderPhotos();
+      renderPhotoInformation(0);
+      getCommentsFor(photos[0].id);
     },
     error: function() {
       console.log('AJAX request failed');
     }
   });
+
+  function renderPhotos() {
+    $('#slides').append(templates.photos({photos: photos}));
+  }
+
+  function renderPhotoInformation(id) {
+    $('section > header').append(templates.photo_information(photos[id]));
+  }
+
+  function getCommentsFor(i) {
+    $.ajax({
+      url: '/comments',
+      data: 'photo_id=' + i,
+      success: function(json) {
+        var comments = json;
+        $('#comments ul').html(templates.comments({comments: comments}));
+      },
+      error: function() {
+        console.log('AJAX request failed');
+      }
+    });
+  }
 });
